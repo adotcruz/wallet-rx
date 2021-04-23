541b3d38f89294f298c785fccfc5f5462bdcf733
Anthony <anthonycruz34@gmail.com>
2021-04-23 18:40:26 -0400

    add web3 initialization to our application

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,7 @@ class App extends React.Component<Record<string, unknown>, undefined> {
     return (
       <div className="app">
         <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
+        <p>Foo to the barz nice</p>
         <img src={reactLogo.default} height="480" />
       </div>
     );
---
+++ src/web3/web3.ts
@@ -0,0 +1,46 @@
+import Web3 from "web3";
+
+interface Web3ExtendedProperties {
+  currentProvider: any;
+}
+
+export interface ExtendedWeb3WindowInterface {
+  ethereum: any;
+  web3: Web3ExtendedProperties;
+}
+
+declare const web3: Web3ExtendedProperties;
+
+const getWeb3 = () =>
+  new Promise((resolve) => {
+    window.addEventListener("load", () => {
+      let currentWeb3;
+
+      if (((window as unknown) as ExtendedWeb3WindowInterface).ethereum) {
+        currentWeb3 = new Web3(
+          ((window as unknown) as ExtendedWeb3WindowInterface).ethereum
+        );
+        try {
+          // Request account access if needed
+          ((window as unknown) as ExtendedWeb3WindowInterface).ethereum.enable();
+          // Acccounts now exposed
+          resolve(currentWeb3);
+        } catch (error) {
+          // User denied account access...
+          alert("Please allow access for the app to work");
+        }
+      } else if (((window as unknown) as ExtendedWeb3WindowInterface).web3) {
+        ((window as unknown) as ExtendedWeb3WindowInterface).web3 = new Web3(
+          (web3 as Web3ExtendedProperties).currentProvider
+        );
+        // Acccounts always exposed
+        resolve(currentWeb3);
+      } else {
+        console.log(
+          "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
+      }
+    });
+  });
+
+export default getWeb3;
