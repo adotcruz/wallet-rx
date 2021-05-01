b51d09e2123c6ba5d84c329b1a2ce5334cb725e7
jw122 <thejuliawu@gmail.com>
2021-05-01 09:58:39 -0700

    render aave holdings, basic ui cleanup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,6 +12,7 @@ export interface Web3Account {}
 
 export interface AppState {
   isVerified: boolean;
+  userReserves: string[];
 }
 
 class App extends React.Component<Record<string, unknown>, AppState> {
@@ -23,13 +24,14 @@ class App extends React.Component<Record<string, unknown>, AppState> {
     super(props);
     this.state = {
       isVerified: false,
+      userReserves: [],
     };
   }
 
   private async setUpWeb3() {
     this.web3 = await getWeb3();
     this.mainAccount = (await this.web3.eth.getAccounts())[0];
-    console.log(this.mainAccount);
+    console.log("MAIN ACCOUNT: ", this.mainAccount);
     this.setState({
       isVerified: true,
     });
@@ -37,31 +39,49 @@ class App extends React.Component<Record<string, unknown>, AppState> {
 
   componentDidMount() {
     this.setUpWeb3();
+    // TODO: fetch real ETH price
+    const ethPriceUsd = 2700;
+    // TODO: only run after this.mainAccount is fetched - at this point it's null
+    this.fetchAave(
+      "0xC33D36523FBF8360792C2c372f5fE457BeeC001f",
+      ethPriceUsd
+    ).then((res) => {
+      console.log("RESULTS ", res);
+    });
   }
 
   public render() {
-    // TODO: fetch real ETH price
-    const ethPriceUsd = 2700;
-    this.fetchAave(this.mainAccount, ethPriceUsd);
     return (
       <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz nice</p>
-        <img src={reactLogo.default} height="480" />
-        {this.state.isVerified ? (
-          <div>
-            <h1>You're verified!!</h1>
-            <h3>welcome {this.mainAccount}</h3>
-          </div>
-        ) : (
-          <h3>not verified :(</h3>
-        )}
+        <div className="user-banner">
+          <h1 className="font-weight-lighter underline--magical-thick">
+            Voltaire
+          </h1>
+          {this.state.isVerified ? (
+            <div>
+              <h4 className="font-weight-light">Hello, {this.mainAccount}</h4>
+            </div>
+          ) : (
+            <h3>not verified :(</h3>
+          )}
+        </div>
+
+        <div className="user-wallet-info shadow-lg p-3 mb-5 bg-white rounded">
+          <h3>
+            Holdings in Aave (Polygon)
+            <img
+              src="https://pbs.twimg.com/profile_images/1186270065085370368/J1YJtvdI.jpg"
+              height="30"
+              className="rounded ml-2"
+            />
+          </h3>
+          {this.state.userReserves}
+        </div>
       </div>
     );
   }
 
   private async fetchAave(address, ethPrice) {
-    console.log("fetching aave for account " + this.mainAccount);
     let lowercaseAddress = address.toLowerCase();
     const v2Reserves = await AaveClient.query({
       query: V2_RESERVES,
@@ -82,7 +102,24 @@ class App extends React.Component<Record<string, unknown>, AppState> {
       usdPriceEth,
       Math.floor(Date.now() / 1000)
     );
-    console.log("v2 user summary: ", v2UserSummary);
+
+    let reserves = v2UserSummary.reservesData;
+    let userReservesArray = [];
+    reserves.forEach((reserve) => {
+      const underlying = reserve.underlyingBalance;
+      const underlyingUsd = reserve.underlyingBalanceUSD;
+      const name = reserve.reserve.name;
+      const summary = underlyingUsd + " USD and " + underlying + " " + name;
+
+      userReservesArray.push(summary);
+    });
+
+    console.log("v2 user summary array: ", userReservesArray);
+
+    this.setState({
+      userReserves: userReservesArray,
+    });
+    return v2UserSummary;
   }
 }
 
